90329ee5a7b31424a5fd17043d1e27e99b63daf9
Andrew Pham <minhanh.vietpham@gmail.com>
2023-03-25 01:10:49 -0500

    update Tanjiro's pic from local path to a url

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import tanjiro from "../images/tanjiro.png";
 
 const Navbar = () => {
 	return (
 		<div className="navBar">
 			<h1>Store Finder</h1>
-			<img src={tanjiro} alt="tanjiro" />
+			<img
+				src="https://i.pinimg.com/originals/0a/53/46/0a5346096266b7db04cac3b5d57b2afd.jpg"
+				alt="tanjiro"
+			/>
 		</div>
 	);
 };
